e23ca9db15e04ed819f8e167dae9a3802f334aa1
DESKTOP-MDOKKPR\tlz <mrt857695@gmail.com>
2024-09-30 16:50:20 +0700

    what is CRLF

---
+++ pages/index.tsx
@@ -0,0 +1,161 @@
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code";
+import { button as buttonStyles } from "@nextui-org/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+import DefaultLayout from "@/layouts/default";
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
+import { Image } from "@nextui-org/image";
+import { useState } from "react";
+
+export default function IndexPage() {
+  const [preview, setPreview] = useState({
+    nameShop: "Drunk 24hr.",
+    detailShop: "We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.",
+    goalShop: "We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.",
+    leftImg: {
+      src: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png",
+      alt: "office content 1"
+    },
+    rightImg: {
+      src: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png",
+      alt: "office content 2"
+    },
+  });
+
+  return (  
+    <DefaultLayout>
+      <section className="bg-white dark:bg-gray-900">
+        <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+          <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">{preview.nameShop}</h2>
+            <p className="mb-4">{preview.detailShop}</p>
+            <p>{preview.goalShop}</p>
+          </div>
+          <div className="grid grid-cols-2 gap-4 mt-8">
+            <img className="w-full rounded-lg" src={preview.leftImg.src} alt={preview.leftImg.alt} />
+            <img className="mt-4 w-full lg:mt-10 rounded-lg" src={preview.rightImg.src} alt={preview.rightImg.alt} />
+          </div>
+        </div>
+      </section>
+      {/* <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+       
+        <div className="marquee h-80">
+          <div className="h-full">
+            <span>You spin me right round, baby. Like a record, baby.</span>
+
+            
+            <span>You spin me right round, baby. Like a record, baby.</span>
+          </div>
+        </div>
+
+      </section> */}
+      <section>
+        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+          <header>
+            <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">Product Collection</h2>
+
+            <p className="mt-4 max-w-md text-gray-500">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque praesentium cumque iure
+              dicta incidunt est ipsam, officia dolor fugit natus?
+            </p>
+          </header>
+
+          <ul className="mt-8 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
+            <li>
+              <a href="#" className="group block overflow-hidden">
+                <img
+                  src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                  alt=""
+                  className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                />
+
+                <div className="relative bg-white pt-3">
+                  <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
+                    Basic Tee
+                  </h3>
+
+                  <p className="mt-2">
+                    <span className="sr-only"> Regular Price </span>
+
+                    <span className="tracking-wider text-gray-900"> £24.00 GBP </span>
+                  </p>
+                </div>
+              </a>
+            </li>
+
+            <li>
+              <a href="#" className="group block overflow-hidden">
+                <img
+                  src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                  alt=""
+                  className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                />
+
+                <div className="relative bg-white pt-3">
+                  <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
+                    Basic Tee
+                  </h3>
+
+                  <p className="mt-2">
+                    <span className="sr-only"> Regular Price </span>
+
+                    <span className="tracking-wider text-gray-900"> £24.00 GBP </span>
+                  </p>
+                </div>
+              </a>
+            </li>
+
+            <li>
+              <a href="#" className="group block overflow-hidden">
+                <img
+                  src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                  alt=""
+                  className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                />
+
+                <div className="relative bg-white pt-3">
+                  <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
+                    Basic Tee
+                  </h3>
+
+                  <p className="mt-2">
+                    <span className="sr-only"> Regular Price </span>
+
+                    <span className="tracking-wider text-gray-900"> £24.00 GBP </span>
+                  </p>
+                </div>
+              </a>
+            </li>
+
+            <li>
+              <a href="#" className="group block overflow-hidden">
+                <img
+                  src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                  alt=""
+                  className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                />
+
+                <div className="relative bg-white pt-3">
+                  <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
+                    Basic Tee
+                  </h3>
+
+                  <p className="mt-2">
+                    <span className="sr-only"> Regular Price </span>
+
+                    <span className="tracking-wider text-gray-900"> £24.00 GBP </span>
+                  </p>
+                </div>
+              </a>
+            </li>
+
+          </ul>
+        </div>
+      </section>
+    </DefaultLayout>
+  );
+}
